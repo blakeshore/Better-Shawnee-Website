2eb171f22c91087a8065000b35b3fb148e68aa76
Vinh Huynh <vinhkhanghuynh03@gmail.com>
2026-04-15 14:01:47 -0500

    edit(recentnews*): fix homepage is now on home page Recent news

--- src/scripts/renderinfo.js
+++ src/scripts/renderinfo.js
@@ -13,7 +13,7 @@ function filterDates() {
 }
 // this function will used to filter date
 function renderInfo(data = newsData) {
-  //  take ID of  newspage
+  //  take ID of newspage
   newspage.innerHTML = data.map(item => `
     <div class="border-b pb-4">
       <a href="${item.link}" class="text-xl font-semibold text-blue-600 hover:underline">
@@ -36,4 +36,25 @@ function loadNews() {
   const article = newsData.find(n => n.link === currentPage);
   document.getElementById("newsTitle").innerText = article.title;
   document.getElementById("newsDate").innerText = "Date " + article.date;
+}
+// render news for front page
+function showRecentNew() {
+  const container = document.getElementById("recentnew");
+  const recent = newsData.slice(0, 4); //  pick 4s news
+  container.innerHTML = recent.map(item => `
+        <div class="flex items-start gap-4 p-4 rounded transition duration-200 hover:bg-base-100 group min-h-[90px]">
+              <svg class="w-10 h-10 text-pink-600 transition-transform duration-200 group-hover:translate-x-1"
+        fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
+          <path d="M4 4h16v16H4z"/>
+          <path d="M8 8h8M8 12h8M8 16h4"/>
+        </svg>
+        <div class="flex flex-col justify-center">
+        <a href="${item.link}" class="font-semibold text-lg hover:underline">
+          ${item.title}
+        </a>
+        <p class="text-sm italic text-base-content/70">${item.date}</p>
+      </div>
+    </div>
+    </div>
+  `).join("");
 }
